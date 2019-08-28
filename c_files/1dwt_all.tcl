aa92eb716fd50032a7d34258e26f178aa57f1ae7
kaxiotis <kostaxiotis@hotmail.com>
2019-08-28 09:18:49 +0300

    some changes

--- c_files/1dwt_all.tcl
+++ c_files/1dwt_all.tcl
@@ -2,46 +2,27 @@
 # then runs synthesis that solution
 
 
-#Initial Design
 
-#open_project -reset dwt_prj
-#add_files dwt_w_names.c
-#add_files -tb dwt_test.c
-#add_files -tb in_images 
-#add_files -tb out_images 
-#add_files -tb out_gold_images
+#sol1
 
-#set_top dwt3D
+open_project -reset dwt_prj1
+add_files non_var_loops.c
+add_files -tb dwt_test.c
+add_files -tb in_images 
+add_files -tb out_images 
+add_files -tb out_gold_images
 
-#open_solution "solution1" -reset
-#set_part  {xc7z010clg400-1}
-#create_clock -period 12
-
-#csim_design
-#csynth_design
+set_top dwt3D
 
 
 
-# #sol1
+open_solution "solution1" -reset
+set_part  {xc7z010clg400-1}
+create_clock -period 11
 
-# open_project -reset dwt_prj1
-# add_files inverse.c
-# add_files -tb dwt_test.c
-# add_files -tb in_images 
-# add_files -tb out_images 
-# add_files -tb out_gold_images
+set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
 
-# set_top dwt3D
-
-
-
-# open_solution "solution1" -reset
-# set_part  {xc7z010clg400-1}
-# create_clock -period 12
-
-# set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
-
-# csynth_design
+csynth_design
 # export_design
 
 
@@ -49,7 +30,7 @@
 #sol2
 
 open_project -reset dwt_prj2
-add_files inverse.c
+add_files non_var_loops.c
 add_files -tb dwt_test.c
 add_files -tb in_images 
 add_files -tb out_images 
@@ -60,12 +41,13 @@ set_top dwt3D
 
 open_solution "solution1" -reset
 set_part  {xc7z010clg400-1}
-create_clock -period 12
+create_clock -period 11
 
 set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
-set_directive_dependence -variable temp -class array -dependent true -direction WAW dwt1D/CALC_LOOP
-set_directive_pipeline dwt1D/CALC_LOOP
-set_directive_dataflow dwt3D
+set_directive_unroll -region DWT_LEVEL_LOOP
+set_directive_unroll -region X_J_LOOP
+set_directive_unroll -region Y_K_LOOP
+set_directive_unroll -region Z_I_LOOP
 
 
 csynth_design
@@ -74,7 +56,7 @@ csynth_design
 #sol3
 
 open_project -reset dwt_prj3
-add_files inverse.c
+add_files non_var_loops.c
 add_files -tb dwt_test.c
 add_files -tb in_images 
 add_files -tb out_images 
@@ -85,39 +67,41 @@ set_top dwt3D
 
 open_solution "solution1" -reset
 set_part  {xc7z010clg400-1}
-create_clock -period 12
+create_clock -period 11
 
 set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
-set_directive_dependence -variable temp -class array -dependent true -direction WAW dwt1D/CALC_LOOP
-set_directive_pipeline -II 4 dwt1D/CALC_LOOP
-set_directive_dataflow dwt3D
-
+set_directive_unroll -region IN_I_LOOP
+set_directive_unroll -region DWT_LEVEL_LOOP
+set_directive_unroll -region X_J_LOOP
+set_directive_unroll -region Y_K_LOOP
+set_directive_unroll -region Z_I_LOOP
+set_directive_unroll -region OUT_I_LOOP
 csynth_design
 # export_design
 
-#sol4
+# #sol4
 
-open_project -reset dwt_prj4
-add_files inverse.c
-add_files -tb dwt_test.c
-add_files -tb in_images 
-add_files -tb out_images 
-add_files -tb out_gold_images
+# open_project -reset dwt_prj4
+# add_files non_var_loops.c
+# add_files -tb dwt_test.c
+# add_files -tb in_images 
+# add_files -tb out_images 
+# add_files -tb out_gold_images
 
-set_top dwt3D
-
-
-open_solution "solution1" -reset
-set_part  {xc7z010clg400-1}
-create_clock -period 12
-
-set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
-set_directive_dependence -variable temp -class array -dependent true -direction WAW dwt1D/CALC_LOOP
-set_directive_pipeline -II 6 dwt1D/CALC_LOOP
-set_directive_dataflow dwt3D
+# set_top dwt3D
 
 
-csynth_design
-# export_design
+# open_solution "solution1" -reset
+# set_part  {xc7z010clg400-1}
+# create_clock -period 12
+
+# set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
+# set_directive_dependence -variable temp -class array -dependent true -direction WAW dwt1D/CALC_LOOP
+# set_directive_pipeline -II 6 dwt1D/CALC_LOOP
+# set_directive_dataflow dwt3D
+
+
+# csynth_design
+# # export_design
 
 exit
