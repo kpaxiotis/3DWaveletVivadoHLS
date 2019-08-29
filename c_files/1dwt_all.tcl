b341cc61167e42745729ea30ddca404e1c194c08
kaxiotis <kostaxiotis@hotmail.com>
2019-08-29 11:45:11 +0300

    Some changes

--- c_files/1dwt_all.tcl
+++ c_files/1dwt_all.tcl
@@ -3,34 +3,34 @@
 
 
 
-# #sol1
+#sol1
 
-# open_project -reset dwt_prj1
-# add_files non_var_loops.c
-# add_files -tb dwt_test.c
-# add_files -tb in_images 
-# add_files -tb out_images 
-# add_files -tb out_gold_images
+open_project -reset dwt_prj1
+add_files single_sub_loops.c
+add_files -tb dwt_test.c
+add_files -tb in_images 
+add_files -tb out_images 
+add_files -tb out_gold_images
 
-# set_top dwt3D
+set_top dwt3D
 
 
 
-# open_solution "solution1" -reset
-# set_part  {xc7z010clg400-1}
-# create_clock -period 11
+open_solution "solution1" -reset
+set_part  {xc7z010clg400-1}
+create_clock -period 11
 
-# set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
+set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
 
-# csynth_design
-# # export_design
+csynth_design
+# export_design
 
 
 
 #sol2
 
 open_project -reset dwt_prj2
-add_files non_var_loops.c
+add_files single_sub_loops.c
 add_files -tb dwt_test.c
 add_files -tb in_images 
 add_files -tb out_images 
@@ -54,53 +54,136 @@ set_directive_loop_flatten dwt3D/OUT_K_LOOP
 csynth_design
 # export_design
 
-# #sol3
+#sol3
 
-# open_project -reset dwt_prj3
-# add_files non_var_loops.c
-# add_files -tb dwt_test.c
-# add_files -tb in_images 
-# add_files -tb out_images 
-# add_files -tb out_gold_images
+open_project -reset dwt_prj3
+add_files single_sub_loops.c
+add_files -tb dwt_test.c
+add_files -tb in_images 
+add_files -tb out_images 
+add_files -tb out_gold_images
 
-# set_top dwt3D
-
-
-# open_solution "solution1" -reset
-# set_part  {xc7z010clg400-1}
-# create_clock -period 11
-
-# set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
-# set_directive_pipeline dwt3D/X_I_LOOP
-# set_directive_pipeline dwt3D/Y_J_LOOP
-# set_directive_pipeline dwt3D/Z_K_LOOP
-
-# csynth_design
-# # export_design
-
-# #sol4
-
-# open_project -reset dwt_prj4
-# add_files non_var_loops.c
-# add_files -tb dwt_test.c
-# add_files -tb in_images 
-# add_files -tb out_images 
-# add_files -tb out_gold_images
-
-# set_top dwt3D
+set_top dwt3D
 
 
-# open_solution "solution1" -reset
-# set_part  {xc7z010clg400-1}
-# create_clock -period 11
+open_solution "solution1" -reset
+set_part  {xc7z010clg400-1}
+create_clock -period 11
 
-# set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
-# set_directive_pipeline -II 3  dwt3D/X_I_LOOP
-# set_directive_pipeline -II 3  dwt3D/Y_J_LOOP
-# set_directive_pipeline -II 3  dwt3D/Z_K_LOOP
+set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
+set_directive_pipeline dwt3D/X_I_LOOP
+set_directive_pipeline dwt3D/Y_J_LOOP
+set_directive_pipeline dwt3D/Z_K_LOOP
+
+csynth_design
+# export_design
+
+#sol4
+
+open_project -reset dwt_prj4
+add_files single_sub_loops.c
+add_files -tb dwt_test.c
+add_files -tb in_images 
+add_files -tb out_images 
+add_files -tb out_gold_images
+
+set_top dwt3D
 
 
-# csynth_design
-# # export_design
+open_solution "solution1" -reset
+set_part  {xc7z010clg400-1}
+create_clock -period 11
+
+set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
+set_directive_pipeline -II 3  dwt3D/X_I_LOOP
+set_directive_pipeline -II 3  dwt3D/Y_J_LOOP
+set_directive_pipeline -II 3  dwt3D/Z_K_LOOP
+
+
+csynth_design
+# export_design
+
+
+
+
+#sol5
+
+open_project -reset dwt_prj5
+add_files single_sub_loops.c
+add_files -tb dwt_test.c
+add_files -tb in_images 
+add_files -tb out_images 
+add_files -tb out_gold_images
+
+set_top dwt3D
+
+
+open_solution "solution1" -reset
+set_part  {xc7z010clg400-1}
+create_clock -period 11
+
+set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
+set_directive_unroll dwt3D/X_I_LOOP
+set_directive_unroll dwt3D/Y_J_LOOP
+set_directive_unroll dwt3D/Z_K_LOOP
+set_directive_unroll dwt3D/IN_K_LOOP
+set_directive_unroll dwt3D/OUT_K_LOOP
+
+
+csynth_design
+# export_design
+
+
+
+#sol6
+
+open_project -reset dwt_prj6
+add_files single_sub_loops.c
+add_files -tb dwt_test.c
+add_files -tb in_images 
+add_files -tb out_images 
+add_files -tb out_gold_images
+
+set_top dwt3D
+
+
+open_solution "solution1" -reset
+set_part  {xc7z010clg400-1}
+create_clock -period 11
+
+set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
+set_directive_array_partition -type block -factor 4 dwt3D temp1->data3D
+set_directive_array_partition -type block -factor 4 dwt3D temp2->data3D
+
+
+csynth_design
+# export_design
+
+
+
+#sol7
+
+open_project -reset dwt_prj7
+add_files single_sub_loops.c
+add_files -tb dwt_test.c
+add_files -tb in_images 
+add_files -tb out_images 
+add_files -tb out_gold_images
+
+set_top dwt3D
+
+
+open_solution "solution1" -reset
+set_part  {xc7z010clg400-1}
+create_clock -period 11
+
+set_directive_interface -mode s_axilite -bundle dwt_io "dwt3D"
+set_directive_array_partition -type complete -dim 0 dwt3D temp1->data3D
+set_directive_array_partition -type complete -dim 0 dwt3D temp2->data3D
+
+
+csynth_design
+# export_design
+
 
 exit
--- c_files/json_read.py
+++ c_files/json_read.py
@@ -8,7 +8,7 @@ latency_avg = []
 sol_list = []
 
 
-for i in range(1, 5):
+for i in range(1, 8):
     directory = "dwt_prj" + str(i) + "/solution1/solution1_data.json"
     with open(directory) as f:
         data = json.load(f)
